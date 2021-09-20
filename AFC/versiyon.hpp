a3f25072756f7540ca6cfc52b85d13570debe29d
Enes Can Turkoglu <enescan58turko@gmail.com>
2021-09-21 00:46:57 +0300

    v0.0.1
    /************************************************************************
     * Versiyon: v0.0.1
     * Tarih       : 21/09/2021
     * Yazan     : Enes Can Turkoglu, Yusuf Tunc
     *
     * Gelistirme Araclari:
     * - STM32CubeIDE : v1.7.0
     * - Firmware     : v1.8.4
     *
     * Versiyon Aciklamalari:
     * ----------------------
     * -> "OtonomUcusKontrolcusu" projesinden alintilanarak
     *       sifirdan "AutonomousFlightController" projesi
     *       STM32CUBE IDE'de olusturuldu.
     * -> Projede kullanilan yazilim dili C++ olarak
     *     degistirildi.
     ***********************************************************************/

---
+++ AFC/versiyon.hpp
@@ -0,0 +1,31 @@
+#ifndef VERSIYON_HPP_
+#define VERSIYON_HPP_
+
+/* Includes autonomous flight controller system and software design */
+
+#define VERSIYON    "0.0.1"
+
+#define MAJOR        0
+#define MINOR        0
+#define BUILD        1
+
+
+/************************************************************************
+ * Versiyon: v0.0.1
+ * Tarih   : 21/09/2021
+ * Yazan   : Enes Can Turkoglu, Yusuf Tunc
+ *
+ * Gelistirme Araclari:
+ * - STM32CubeIDE : v1.7.0
+ * - Firmware     : v1.8.4
+ *
+ * Versiyon Aciklamalari:
+ * ----------------------
+ * -> "OtonomUcusKontrolcusu" projesinden alintilanarak
+ *    sifirdan "AutonomousFlightController" projesi
+ *    STM32CUBE IDE'de olusturuldu.
+ * -> Projede kullanilan yazilim dili C++ olarak
+ *    degistirildi.
+ ***********************************************************************/
+
+#endif /* VERSIYON_HPP_ */
